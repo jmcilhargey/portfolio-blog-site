e455bd56156b43c3e31db2fadea3b3e1726519aa
jmcilhargey <jmcilhargey@gmail.com>
2016-08-16 17:01:53 -0700

    Revert "Revert "Minified js/css, added article on d3.js, fixed xml file path""
    This reverts commit 57885f061a3c115e7e93911f118dca7e95ddd167.

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 
 var mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, function(err) {
+mongoose.connect("mongodb://jmcilhargey:qualcom1@ds059365.mlab.com:59365/joesdb", function(err) {
 	if (err) {
 		throw new Error("Failed to connect to MongoDB");
 	} else {
