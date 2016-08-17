73cb9c4376af9dd5e0422b2b91cd9cd4054aad98
jmcilhargey <jmcilhargey@gmail.com>
2016-08-17 11:24:29 -0700

    Adding SSL functionality

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 "use strict";
 
 var express = require("express");
+var fs = require("fs");
+var https = require("https");
 var router = require("./api")
 
 var app = express();
+
+console.log();
+
+var options = {
+	ca: fs.readFileSync(process.env.SSL_CA),
+	key: fs.readFileSync(process.env.SSL_KEY),
+	cert: fs.readFileSync(process.env.SSL_CERT)
+};
 
 require("./database");
 require("./seed");
@@ -12,6 +22,6 @@ app.use("/", express.static("public"));
 
 app.use("/api", router);
 
-app.listen(process.env.PORT || 3000, function() {
-	console.log("Server running on port 3000");
+https.createServer(options, app).listen(process.env.PORT || 3000, function() {
+	console.log("Server listening on port " + (process.env.PORT || 3000));
 });
