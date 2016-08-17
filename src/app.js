39eb184c801236e920a4e328eeb9efc681ac39ae
jmcilhargey <jmcilhargey@gmail.com>
2016-08-17 11:40:40 -0700

    Moving SSL cert verification to Heroku

--- src/app.js
+++ src/app.js
@@ -1,19 +1,9 @@
 "use strict";
 
 var express = require("express");
-var fs = require("fs");
-var https = require("https");
 var router = require("./api")
 
 var app = express();
-
-console.log();
-
-var options = {
-	ca: fs.readFileSync(process.env.SSL_CA),
-	key: fs.readFileSync(process.env.SSL_KEY),
-	cert: fs.readFileSync(process.env.SSL_CERT)
-};
 
 require("./database");
 require("./seed");
@@ -22,6 +12,7 @@ app.use("/", express.static("public"));
 
 app.use("/api", router);
 
-https.createServer(options, app).listen(process.env.PORT || 3000, function() {
-	console.log("Server listening on port " + (process.env.PORT || 3000));
+app.listen(process.env.PORT || 3000, function() {
+
+	console.log("Server listening on port: " + (process.env.PORT || 3000));
 });
