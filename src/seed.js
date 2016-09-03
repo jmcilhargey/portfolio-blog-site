b009163c3a9f413aa0eb684a52e283d0f0d28cdf
jmcilhargey <jmcilhargey@gmail.com>
2016-09-03 11:58:11 -0700

    Added project images as local assets

--- src/seed.js
+++ src/seed.js
@@ -6,103 +6,116 @@ var projects = [
 	{
 		"title": "Local Nightlife",
 		"link": "https://local-nightlife.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/nightlife_app.png",
-		"desc": "Uses Yelp's API and Google OAuth to allow users to search for events by city. Authenticated users can to RSVP for events and see a list of other attendees. Built on the MEAN stack."
+		"pic": "../images/nightlife_app.png",
+		"desc": "An event finding app where users search for events by city with the Yelp API. Users can authenticate with Google to sign up for events and see other users who are going. Users can select events and most efficient route for walking or driving via the Google Directions API. HTML/CSS, Bootstrap, Javascript, AngularJS, Node.js, Express, MongoDB, Mongoose, OAuth."
 	},
 	{
 		"title": "Gold Medals",
 		"link": "https://olympic-medals.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/olympics.png",
-		"desc": "Olympic medal tracker built with the MEAN stack that stores gold medal winners by event. A d3.js graph breaks down the winners by country and date as the games develop"
+		"pic": "../images/olympics.png",
+		"desc": "Olympic medal tracker that stores gold medal winners by event. Users can break down the winners by country and event to see graphical results as the games develop. HTML/CSS, Javascript-ES6, d3.js, AngularJS, Node.js, Express, MongoDB, Mongoose, Redis."
 	},
 	{
 		"title": "Easy Polls",
 		"link": "https://voting-polling-app.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/votingapp.png",
-		"desc": "An app to create, vote, and share polls on any topic. A d3.js bar chart that gives a breakdown of votes for each poll. Users can also randomly generate polls Built on the MEAN stack with authentication through Github."
+		"pic": "../images/votingapp.png",
+		"desc": "A polling app that allows authenticated users to create, vote, and share polls on any topic. Each poll can be voted on once and results can be viewed in graph format. Users can also randomly generate polls on a topic of choice. HTML/CSS, Bootstrap, Javascript, AngularJS, Node.js, Express, MongoDB. "
+	},
+	{
+		"title": "Tracking Leicester's Victory",
+		"link": "https://www.joemcilhargey.com/articles/pathfinding-with-yelp-and-google-directions-apis/",		
+		"pic": "../images/epl-project.png",
+		"desc": "A research project and tutorial built with d3.js tracks how Leicester City FC won the 2015-2016 Premier League. Interactive graphs show the relationship between goals, points, and goal differential over the course of the season. HTML/CSS, Javascript, d3.js"
+	},
+	{
+		"title": "A Multi-Talented Slack Bot",
+		"link": "https://www.joemcilhargey.com/articles/building-a-talented-slack-bot-in-node/",
+		"pic": "../images/slack-bot.png",
+		"desc": "A Slack bot assistant that can fetch current news and stock prices, calculate distances and estimated travel times, get the local forecast, and call an Uber to pick you up. Node.js, WebSockets, HTTP/HTTPS, Google, Quandl, OpenWeather, and Uber APIs"
+	},
+	{
+		"title": "Image Search Tool",
+		"link": "https://image-search-tool.herokuapp.com/",		
+		"pic": "../images/imagesearchtool.png",
+		"desc": "A microservice API that allows users to access the Google Image Search API by keyword and view top images. Results are paginated and search history stored in DB. HTML/CSS, Bootstrap, Javascript, Node.JS, Express, MongoDB."
+	},
+	{
+		"title": "URL Shortener",
+		"link": "https://url-service.herokuapp.com/",		
+		"pic": "../images/urlshortener.png",
+		"desc": "A microservice API that shortens URLs via random character generation and stores route and original address in DB. HTML/CSS, Bootstrap, Javascript, Node.JS, Express, MongoDB."
 	},
 	{
 		"title": "Simon",
 		"link": "http://codepen.io/jmcilhargey/pen/VexKqo",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/simon.png",
-		"desc": "The classic game where the player must replicate the computer's turn. Make it to 15 turns to win! Built with HTML/CSS, Javascript."		
+		"pic": "../images/simon.png",
+		"desc": "A replica of the classic game where the player must replicate the computer's turn. HTML/CSS, Bootstrap, Javascript, jQuery"		
 	},
 	{
-		"title": "Tic Tac Toe",
+		"title": "Tic-Tac-Toe",
 		"link": "http://codepen.io/jmcilhargey/pen/XXebga",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/tic-tac-toe.png",
-		"desc": "An unbeatable version of tic-tac-toe that uses the minimax algorithm to calculate the best move. Try it! Built with HTML/CSS, Javascript."		
+		"pic": "../images/tic-tac-toe.png",
+		"desc": "An unbeatable version of tic-tac-toe that uses the minimax algorithm to calculate the best move. HTML/CSS, Bootstrap, Javascript, jQuery."		
+	},
+	{
+		"title": "Check File Size",
+		"link": "https://filesize-service.herokuapp.com/",		
+		"pic": "../images/filesizeservice.png",
+		"desc": "A microservice API that checks upload file size and returns JSON results. HTML/CSS, Bootstrap, Javascript, Node.JS, Express, MongoDB."
+	},
+	{
+		"title": "Convert Time Stamp",
+		"link": "https://timestamp-service-api.herokuapp.com/",		
+		"pic": "../images/timestampservice.png",
+		"desc": "A microservice API that accepts date in standard or UNIX format and returns JSON with both. HTML/CSS, Bootstrap, Javascript, Node.JS, Express, MongoDB."
+	},	
+	{
+		"title": "Request Header",
+		"link": "https://header-parser-microservice.herokuapp.com/",		
+		"pic": "../images/requestheaderservice.png",
+		"desc": "A microservice API that returns JSON with client header information. HTML/CSS, Bootstrap, Javascript, Node.JS, Express, MongoDB."
 	},	
 	{
 		"title": "Stories",
 		"link": "https://stories-railsapp.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/stories-screen.jpeg",
-		"desc": "A travel journal app built on Rails that stores sign ups in a database."
+		"pic": "../images/stories-screen.jpeg",
+		"desc": "A travel journal app that stores sign ups in a database. HTML/CSS, Javascript, jQuery, Ruby on Rails."
 	},
 	{
 		"title": "Bass Music",
 		"link": "https://bassmusic-railsapp.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/portfolio/images/bass-music-screen.jpeg",
-		"desc": "A music catalog built on Rails that uses associations to show albums and tracks."		
+		"pic": "../images/bass-music-screen.jpeg",
+		"desc": "A music catalog built on Rails that uses associations to show albums and tracks. HTML/CSS, Javascript, jQuery, Ruby on Rails."		
 	},
 	{
 		"title": "Newstand",
 		"link": "https://newstand-railsapp.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/newsapp-screen.jpeg",
-		"desc": "A news posting project built in Rails with sign up, login, and role based authorization for posting and editing articles."		
+		"pic": "../images/newsapp-screen.jpeg",
+		"desc": "A news posting project built in Rails with sign up, login, and role based authorization for posting and editing articles. HTML/CSS, Javascript, jQuery, Ruby on Rails."		
 	},
 	{
 		"title": "Calculator",
 		"link": "http://codepen.io/jmcilhargey/pen/yegaao",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/calculator.png",
-		"desc": "A simple calculator that stores values and uses a switch statement. Built with HTML/CSS, Javascript (jQuery)."		
+		"pic": "../images/calculator.png",
+		"desc": "A simple in-browser calculator that performs basic math operations. HTML/CSS, Javascript, jQuery."		
 	},
 	{
 		"title": "Pomodoro Timer",
 		"link": "http://codepen.io/jmcilhargey/full/dGNWOM/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/pomodoro.png",
-		"desc": "Work in intervals with the pomodoro technique. Built on HTML/CSS, Javascript (AngularJS)."		
+		"pic": "../images/pomodoro.png",
+		"desc": "A timer where users can set intervals for work and break times. HTML/CSS, Javascript, AngularJS."		
 	},
 	{
 		"title": "Bookmarks",
 		"link": "https://bookmarks-railsapp.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/bookmarks-screen.jpeg",
-		"desc": "A book review site built on Rails that pulls reviews based on book associations."		
+		"pic": "../images/bookmarks-screen.jpeg",
+		"desc": "A book review site built on Rails that pulls reviews based on book associations. HTML/CSS, Javascript, jQuery, Ruby on Rails."		
 	},
 	{
 		"title": "Twitch Coder Stream",
 		"link": "http://codepen.io/jmcilhargey/full/zrzWOb/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/twitch-stream.png",
-		"desc": "An app that uses the Twitch API to list and link programming streamers. Built with HTML/CSS, Javscript (AngularJS)."
-	},
-		{
-		"title": "Image Search Tool",
-		"link": "https://image-search-tool.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/imagesearchtool.png",
-		"desc": "A microservice using Google Image Search API to return images, paginate, and save history to MongoDB."
-	},
-		{
-		"title": "URL Shortener",
-		"link": "https://url-service.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/urlshortener.png",
-		"desc": "A microservice that shortens URLs using a random character generator and saves new URL route to MongoDB."
-	},
-		{
-		"title": "Check File Size",
-		"link": "https://filesize-service.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/filesizeservice.png",
-		"desc": "A microservice that checks the size of a file upload and returns JSON."
-	},
-		{
-		"title": "Convert Time Stamp",
-		"link": "https://timestamp-service-api.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/timestampservice.png",
-		"desc": "A microservice that accepts date in standard or UNIX format and returns JSON with both."
-	},	{
-		"title": "Request Header",
-		"link": "https://header-parser-microservice.herokuapp.com/",		
-		"pic": "https://dl.dropboxusercontent.com/u/63260308/Portfolio/Images/requestheaderservice.png",
-		"desc": "A microservice that returns JSON with client header information."
+		"pic": "../images/twitch-stream.png",
+		"desc": "An app that uses the Twitch.tv API to list and link programming streamers. HTML/CSS, Javascript, AngularJS."
 	}
 ];
 
